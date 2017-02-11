7fa9e7602c15c59b7b9d8d33751c3f57dd89cfd2
Arpan <sahoo.arpan7@gmail.com>
2017-02-11 15:31:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,21 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('article-one', function (req, res) {
+  res.sendFile('Article one requested and served here');
+});
+
+app.get('article-two', function (req, res) {
+res.sendFile('Article two requested and served here');
+});
+
+app.get('article-three', function (req, res) {
+  res.sendFile('Article three requested and served here');
 });
 
 
