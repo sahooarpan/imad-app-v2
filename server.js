b8955a4570f3bcf25832d6e4e15a41b095eea4bc
Arpan <sahoo.arpan7@gmail.com>
2017-02-11 16:13:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-res.sendFile('Article two requested and served here');
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile('Article three requested and served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
 
 
